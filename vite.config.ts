d9e10a57c41d685daf01beee30d4e8c06fd859e8
adewka300 <109631649+adewka300@users.noreply.github.com>
2026-05-17 15:42:12 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,24 @@
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
-import path from 'node:path';
+import path from 'path';
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint2';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), eslint()],
+  plugins: [react(), eslint()],
   resolve: {
     alias: {
-      '@': path.resolve('./src'),
-      '@shared': path.resolve('./src/shared'),
-      '@features': path.resolve('./src/features'),
-      '@widgets': path.resolve('./src/widgets'),
-      '@pages': path.resolve('./src/pages'),
-      '@entities': path.resolve('./src/entities'),
+      '@': path.resolve(__dirname, './src'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@widgets': path.resolve(__dirname, './src/widgets'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@entities': path.resolve(__dirname, './src/entities'),
     },
+  },
+  server: {
+    allowedHosts: ['www.mytropa.ru', 'mytropa.ru'],
+    host: '0.0.0.0',
+    port: 5173,
   },
 });
